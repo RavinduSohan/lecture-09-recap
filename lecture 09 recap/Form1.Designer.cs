e53b33f0c7d978e403a21410fe40366cc7ff0287
hp <hp@DESKTOP-GMHV9JJ>
2024-03-22 23:08:06 +0530

    Add project files.

---
+++ lecture 09 recap/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace lecture_09_recap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblname = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblpassword = new System.Windows.Forms.Label();
+            this.lblconfirmpassword = new System.Windows.Forms.Label();
+            this.textBoxname = new System.Windows.Forms.TextBox();
+            this.textBoxconfirmpassword = new System.Windows.Forms.TextBox();
+            this.textBoxpassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(242, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "University Registration Page";
+            // 
+            // lblname
+            // 
+            this.lblname.AutoSize = true;
+            this.lblname.Location = new System.Drawing.Point(91, 69);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(35, 13);
+            this.lblname.TabIndex = 1;
+            this.lblname.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(91, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "DOB";
+            // 
+            // lblpassword
+            // 
+            this.lblpassword.AutoSize = true;
+            this.lblpassword.Location = new System.Drawing.Point(91, 151);
+            this.lblpassword.Name = "lblpassword";
+            this.lblpassword.Size = new System.Drawing.Size(53, 13);
+            this.lblpassword.TabIndex = 3;
+            this.lblpassword.Text = "Password";
+            // 
+            // lblconfirmpassword
+            // 
+            this.lblconfirmpassword.AutoSize = true;
+            this.lblconfirmpassword.Location = new System.Drawing.Point(91, 200);
+            this.lblconfirmpassword.Name = "lblconfirmpassword";
+            this.lblconfirmpassword.Size = new System.Drawing.Size(91, 13);
+            this.lblconfirmpassword.TabIndex = 4;
+            this.lblconfirmpassword.Text = "Confirm Password";
+            // 
+            // textBoxname
+            // 
+            this.textBoxname.Location = new System.Drawing.Point(246, 69);
+            this.textBoxname.Name = "textBoxname";
+            this.textBoxname.Size = new System.Drawing.Size(253, 20);
+            this.textBoxname.TabIndex = 5;
+            // 
+            // textBoxconfirmpassword
+            // 
+            this.textBoxconfirmpassword.Location = new System.Drawing.Point(246, 193);
+            this.textBoxconfirmpassword.Name = "textBoxconfirmpassword";
+            this.textBoxconfirmpassword.Size = new System.Drawing.Size(253, 20);
+            this.textBoxconfirmpassword.TabIndex = 6;
+            // 
+            // textBoxpassword
+            // 
+            this.textBoxpassword.Location = new System.Drawing.Point(246, 148);
+            this.textBoxpassword.Name = "textBoxpassword";
+            this.textBoxpassword.Size = new System.Drawing.Size(253, 20);
+            this.textBoxpassword.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(320, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(246, 109);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(253, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxpassword);
+            this.Controls.Add(this.textBoxconfirmpassword);
+            this.Controls.Add(this.textBoxname);
+            this.Controls.Add(this.lblconfirmpassword);
+            this.Controls.Add(this.lblpassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblname);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblpassword;
+        private System.Windows.Forms.Label lblconfirmpassword;
+        private System.Windows.Forms.TextBox textBoxname;
+        private System.Windows.Forms.TextBox textBoxconfirmpassword;
+        private System.Windows.Forms.TextBox textBoxpassword;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ lecture 09 recap/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lecture_09_recap
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string name=textBoxname.Text;
+            string password=textBoxpassword.Text;
+            string confirmpassword=textBoxconfirmpassword.Text;
+            string dob=dateTimePicker1.Value.ToString("yyyy-MM-dd");
+
+            //creating the sql query
+            string query = "INSERT INTO Student(Name,dob,password) VALUES ('{name}','{dob}','{password}');";
+            SqlCommand cmd = new SqlCommand(query,con1);
+
+            //excecution of the command
+            try
+            {
+                con1.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Successful data entry!");
+                con1.Close(); //to re-enter data without exceptions
+            }
+            catch(SqlException sqlex)
+            {
+                MessageBox.Show(sqlex.Message);
+            }
+        }
+    }
+}
